7e1d0877e91b396810e427202814e4777b98db80
Alexander Repeta <alexander.repeta@gmail.com>
2024-04-03 17:44:32 +0300

    prepare examples

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,16 @@
-import { useState } from "react";
 import ClickCounter from "../ClickCounter/ClickCounter";
-import Toggler from "../Toggler/Toggler";
+// import Timer from "../Timer/Timer";
+// import Reader from "../Reader/Reader";
+// import articles from "../../articles.json";
 import css from "./App.module.css";
-import Reader from "../Reader/Reader";
-import articles from "../../articles.json";
 
 export default function App() {
-  const [clicks, setClicks] = useState(0);
-
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-
   return (
     <div className={css.container}>
-      <h1>State in React</h1>
-      <hr />
-      <Reader items={articles} />
-      <hr />
-      <ClickCounter value={clicks} onUpdate={handleClick} />
-      <ClickCounter value={clicks} onUpdate={handleClick} />
-      <ClickCounter value={clicks} onUpdate={handleClick} />
-      <hr />
-      <Toggler />
-      <Toggler />
-      <Toggler />
+      <h1>Effects in React</h1>
+      <ClickCounter />
+      {/* <Timer /> */}
+      {/* <Reader items={articles} /> */}
     </div>
   );
 }
--- src/components/ClickCounter/ClickCounter.jsx
+++ src/components/ClickCounter/ClickCounter.jsx
@@ -1,3 +1,16 @@
-export default function ClickCounter({ value, onUpdate }) {
-  return <button onClick={onUpdate}>Clicks: {value}</button>;
+import { useState } from "react";
+
+export default function ClickCounter() {
+  const [clicks, setClicks] = useState(0);
+  const updateClicks = () => setClicks(clicks + 1);
+
+  const [date, setDate] = useState(Date.now());
+  const updateDate = () => setDate(Date.now());
+
+  return (
+    <>
+      <button onClick={updateClicks}>Clicks: {clicks}</button>
+      <button onClick={updateDate}>Date: {date}</button>
+    </>
+  );
 }
