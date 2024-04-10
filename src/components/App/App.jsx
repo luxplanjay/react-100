3269674b61cd5b3c044a514061eccb763550b311
Alexander Repeta <alexander.repeta@gmail.com>
2024-04-10 14:32:19 +0300

    prepare examples

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from "react";
 import UserForm from "../UserForm/UserForm";
-import TextInput from "../TextInput/TextInput";
-import LangSwitcher from "../LangSwitcher/LangSwitcher";
-import OrderForm from "../OrderForm/OrderForm";
 import css from "./App.module.css";
 
 export default function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [lang, setLang] = useState("uk");
-
   const handleAddUser = (newUser) => {
     console.log("handleAddUser", newUser);
   };
 
-  const makeOrder = (newOrder) => {
-    console.log("makeOrder", newOrder);
-  };
-
   return (
     <div className={css.container}>
-      <h1>Forms in React</h1>
-
+      <h1>Forms with Formik</h1>
       <UserForm onAdd={handleAddUser} />
-
-      <TextInput value={inputValue} onType={setInputValue} />
-      <p>Text: {inputValue}</p>
-
-      <LangSwitcher value={lang} onChangeLang={setLang} />
-      <p>Selected lang: {lang}</p>
-
-      <OrderForm onOrder={makeOrder} />
     </div>
   );
 }
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,37 +1,32 @@
-import { useId } from "react";
 import css from "./UserForm.module.css";
 
 export default function UserForm({ onAdd }) {
-  const usernameId = useId();
-  const roleId = useId();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-
-    onAdd({
-      username: form.elements.username.value,
-      role: form.elements.role.value,
-    });
-
-    form.reset();
-  };
-
   return (
-    <form className={css.form} onSubmit={handleSubmit}>
+    <form className={css.form}>
       <div className={css.group}>
-        <label htmlFor={usernameId}>Username</label>
-        <input type="text" name="username" id={usernameId} />
+        <label>Username:</label>
+        <input type="text" name="username" required />
       </div>
 
       <div className={css.group}>
-        <label htmlFor={roleId}>Role</label>
-        <select name="role" id={roleId}>
+        <label>Email:</label>
+        <input type="email" name="email" required />
+      </div>
+
+      <div className={css.group}>
+        <label>Role:</label>
+        <select name="role" required>
           <option value="guest">Guest</option>
           <option value="user">User</option>
           <option value="admin">Admin</option>
         </select>
       </div>
+
+      <div className={css.group}>
+        <label>Comment:</label>
+        <textarea name="comment"></textarea>
+      </div>
+
       <button type="submit">Submit</button>
     </form>
   );
